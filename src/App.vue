de3beb4274709a62e1c37f01b412b3e1943eec9f
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-12-09 00:33:22 +0100

    feat: removing unused navigation tabs

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,7 @@
           </transition>
           <ul class="desktop-nav">
             <li><RouterLink to="/">Home</RouterLink></li>
-            <li><RouterLink to="/about">More About Me</RouterLink></li>
             <li><RouterLink to="/works">My Works</RouterLink></li>
-            <li><RouterLink to="/contact">Contact</RouterLink></li>
           </ul>
         </nav>
       </div>
